944d49c30422c3236e6ec20966c2b052bb47d7f4
Matthew Carter <m@ahungry.com>
2020-04-18 23:21:50 -0400

    Start on Iup

---
+++ target/iup/iup.c
@@ -0,0 +1,13 @@
+#include <iup.h>
+#include <string.h>
+#include <stdio.h>
+
+int
+main (int argc, char *argv[])
+{
+  int result = IupOpen (NULL, NULL);
+
+  printf ("Success\n");
+
+  return 0;
+}
