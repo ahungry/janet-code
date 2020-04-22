a023da5b93d97b6f824934dcf7acf5cdfa8ee49e
Matthew Carter <m@ahungry.com>
2020-04-22 00:36:26 -0400

    Pull in im for linux

--- target/super/get-iup-linux-files.sh
+++ target/super/get-iup-linux-files.sh
@@ -8,7 +8,14 @@ cd build/linux/iup
 wget 'https://sourceforge.net/projects/iup/files/3.28/Linux%20Libraries/iup-3.28_Linux50_64_lib.tar.gz' -O iup.tar.gz
 
 tar xzvf iup.tar.gz
-#wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Linux%20Libraries/im-3.13_Linux415_64_lib.tar.gz'
+
+mkdir -p build/linux/im
+cd build/linux/im
+
+wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Linux%20Libraries/im-3.13_Linux415_64_lib.tar.gz' -O im.tar.gz
+
+tar xzvf im.tar.gz
+
 #wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Linux%20Libraries/cd-5.12_Linux44_64_lib.tar.gz'
 
 
