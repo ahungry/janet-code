23179907740d4930c5252198aef7151d2eadd687
Matthew Carter <m@ahungry.com>
2020-04-22 00:39:56 -0400

    fix iup pull

--- target/super/get-iup-windows-files.sh
+++ target/super/get-iup-windows-files.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-# # GNU/Linux files
-# mkdir -p build/linux/iup
-# cd build/linux/iup
-
-# # .a and .so files
-# wget 'https://sourceforge.net/projects/iup/files/3.28/Linux%20Libraries/iup-3.28_Linux50_64_lib.tar.gz' -O iup.tar.gz
-
-# tar xzvf iup.tar.gz
-# #wget 'https://sourceforge.net/projects/imtoolkit/files/3.13/Linux%20Libraries/im-3.13_Linux415_64_lib.tar.gz'
-# #wget 'https://sourceforge.net/projects/canvasdraw/files/5.12/Linux%20Libraries/cd-5.12_Linux44_64_lib.tar.gz'
+top=$(pwd)
 
 
 mkdir -p build/win/iup
@@ -22,6 +13,8 @@ wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Static
 
 unzip iup.zip
 #wget 'https://sourceforge.net/projects/iup/files/3.28/Windows%20Libraries/Dynamic/iup-3.28_Win64_dllw6_lib.zip'
+
+cd $top
 
 mkdir -p build/win/im
 cd build/win/im
